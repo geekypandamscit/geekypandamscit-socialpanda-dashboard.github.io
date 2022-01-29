659233ac1a16ad7251dae4d959d3594d7af1bd85
Social Panda <geekypandamscit@gmail.com>
2022-01-29 18:31:56 +0530

    make it better

--- mainpagetemplate.py
+++ mainpagetemplate.py
@@ -1,7 +1,7 @@
 import dash                              
 import dash_html_components as html
 import dash_core_components as dcc
-from dash.dependencies import Output, Input
+#from dash.dependencies import Output, Input
 #from dash_extensions import Lottie
 import dash_bootstrap_components as dbc  
 #import plotly.express as px
